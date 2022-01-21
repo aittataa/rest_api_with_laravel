b54c4f2a6c95bbd3ebbdfbeaa081609c5fe057c9
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-21 16:47:25 +0100

    commit

--- database/migrations/2022_01_21_151107_create_categories_table.php
+++ database/migrations/2022_01_21_151107_create_categories_table.php
@@ -14,13 +14,17 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('tbl_categories', function (Blueprint $table) {
-            $table->id_category();
+            //$table->id();
+            $table->increments("id_category");
             $table->string("category_name", 100);
             $table->text("category_image", 100);
-            $table->bit("category_status", 1);
-            $table->timestamps("create_at");
-            $table->timestamps("update_at");
-            $table->timestamps("delete_at");
+            $table->tinyInteger("category_status")->default(1);
+            // $table->timestamp();
+            // $table->softDeletes();
+
+            $table->timestamp("created_at")->useCurrent();
+            $table->timestamp("updated_at")->useCurrent();
+            $table->timestamp("deleted_at")->nullable();
         });
     }
 
