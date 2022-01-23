05fe9f3e3d7cc19a600d3c61777cafaa23063b0e
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-23 14:05:15 +0100

    commit

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,35 +5,67 @@ namespace App\Http\Controllers;
 use App\Models\Categories;
 use App\Http\Requests\StoreCategoriesRequest;
 use App\Http\Requests\UpdateCategoriesRequest;
+use App\Http\Resources\CategoriesResource;
 
 class CategoriesController extends Controller
 {
     public function index()
     {
-        return Categories::all();
+        return CategoriesResource::collection(Categories::all());
     }
 
-    public function show($id)
+    public function show(Categories $category, $id)
     {
-        return Categories::findOrFail($id);
+        $category = Categories::findOrFail($id);
+        return [
+            "id" => $category->id,
+            "category_name" => $category->category_name,
+            "category_image" => $category->category_image,
+            "category_status" => $category->category_status,
+            "created_at" => $category->created_at,
+            "updated_at" => $category->updated_at,
+            //"deleted_at" => $category->deleted_at,
+        ];
     }
 
     public function store(StoreCategoriesRequest $request)
     {
-        return Categories::create($request->all());
+        $category = Categories::create($request->all());
+        $category = Categories::findOrFail($category->id);
+        return [
+            "id" => $category->id,
+            "category_name" => $category->category_name,
+            "category_image" => $category->category_image,
+            "category_status" => $category->category_status,
+            "created_at" => $category->created_at,
+            "updated_at" => $category->updated_at,
+            //"deleted_at" => $category->deleted_at,
+        ];
+        //return new CategoriesResource($category);
     }
 
-    public function update(UpdateCategoriesRequest $request, $id)
+    public function update(UpdateCategoriesRequest $request, Categories $category)
     {
-        $category = Categories::findOrFail($id);
+        $category = Categories::findOrFail($category->id);
         $category->update($request->all());
-        return $category;
+        return [
+            "id" => $category->id,
+            "category_name" => $category->category_name,
+            "category_image" => $category->category_image,
+            "category_status" => $category->category_status,
+            "created_at" => $category->created_at,
+            "updated_at" => $category->updated_at,
+            //"deleted_at" => $category->deleted_at,
+        ];
+        //return new CategoriesResource($category);
     }
 
-    public function destroy($id)
+    public function destroy(Categories $category)
     {
-        $category = Categories::findOrFail($id);
         $category->delete();
-        return response("Successfully Deleted");
+        return [
+            "message" => "Successfully Deleted",
+        ];
+        //return response("Successfully Deleted");
     }
 }
---
+++ app/Http/Resources/CategoriesResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoriesResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return  [
+            "id" => $this->id,
+            "category_name" => $this->category_name,
+            "category_image" => $this->category_image,
+            "category_status" => $this->category_status,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "deleted_at" => $this->deleted_at,
+        ];
+    }
+}
