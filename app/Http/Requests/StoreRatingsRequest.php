1ecf4c8421a8a29a76606e749a173f3f5967b9b6
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2022-01-25 21:39:07 +0100

    commit

--- app/Http/Requests/StoreRatingsRequest.php
+++ app/Http/Requests/StoreRatingsRequest.php
@@ -8,7 +8,7 @@ class StoreRatingsRequest extends FormRequest
 {
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     public function rules()
--- database/migrations/2022_01_25_200132_create_ratings_table.php
+++ database/migrations/2022_01_25_200132_create_ratings_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Users;
+use App\Models\Wallpapers;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
